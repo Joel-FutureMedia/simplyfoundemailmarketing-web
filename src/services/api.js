e9b287d4828553d85658e4a481e99214e916da05
Joel-FutureMedia <joel.kalimbwe@futuremedia.com.na>
2026-02-16 16:18:02 +0200

    push

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8585/api'
+//const API_BASE_URL = 'http://localhost:8585/api'
+const API_BASE_URL = 'https://emailmarketin.simplyfound.com.na/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
